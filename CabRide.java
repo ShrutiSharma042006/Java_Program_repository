2324ad00775b2f4c2b3c0f12a27907e7b7441951
shruti sharma <shruti.s.00107@gmail.com>
2026-03-25 22:13:14 +0530

    updated code

--- CabRide.java
+++ CabRide.java
@@ -1,18 +1,18 @@
+
 public class CabRide {
-
-    public static void main(String[] args){
-
+    CabRide(int distance) {
+        int rem;
+        if (distance < 5) {
+            // System.out.println(distance*50);
+            System.out.println("the amount  is " + (distance * 50));
+        } else {
+            rem = distance - 5;
+            System.out.println("teh amount is " + ((5 * 50) + (rem * 30)));
+        }
     }
 
-    CabRide(int dist d){
-    distance =d;
-    int rem=d-5;
-    if(distance>5){
-        fare=25+(distance-5)*30;
+    public static void main(String[] args) {
+        CabRide c = new CabRide(3);
+
     }
-    else{
-        fare distance*50;
-    } if ()
-    System.out.println("distance="distance+"fare"+fare);
-}
 }
