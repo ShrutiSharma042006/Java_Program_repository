3822a98c6d189c9ca03c09a159d62c72c74614c3
shruti sharma <shruti.s.00107@gmail.com>
2026-03-25 21:44:54 +0530

    updated code

--- method_overloading2.java
+++ method_overloading2.java
@@ -1,17 +1,18 @@
 public class method_overloading2 {
     public static void main(String[] args) {
-        area();
+        area(30);
+
     }
 
     static void area(int r) {
-        System.out.println("area of circle is " + (3.14 * r * r));
+        System.out.println("Area of circle is " + (3.14 * r * r));
     }
 
     static void area(int b, int h) {
-        System.out.println("area of triangle is " + (0.5 * b * h));
+        System.out.println("Area of triangle is " + (0.5 * b * h));
     }
 
-    static void area(int l, int b) {
-        System.out.println("area of rectangle is " + (l * b));
+    static void area(double l, double b) {
+        System.out.println("Area of rectangle is " + (l * b));
     }
 }
