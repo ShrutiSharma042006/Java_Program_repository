3821c205bffef9956c9ce0f2c43f4f5bfcfa6a0d
shruti sharma <shruti.s.00107@gmail.com>
2026-03-26 22:39:06 +0530

    updated code

---
+++ LCM.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LCM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int max = 0;
+        if (a > b) {
+            max = a;
+        } else {
+            max = b;
+        }
+        while (true) {
+            if (max % a == 0 && max % b == 0) {
+                System.out.println("LCM of" + a + "and" + b + "is" + max);
+                break;
+
+            } else {
+                max++;
+            }
+        }
+    }
+}
