300f49ff0d352c050ec39b77c8e45a0cc90d4191
shruti sharma <shruti.s.00107@gmail.com>
2026-04-21 15:58:56 +0530

    program

---
+++ uppertolower.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class uppertolower {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        name = name.toLowerCase();
+        System.out.println("result" + name);
+
+    }
+
+}
